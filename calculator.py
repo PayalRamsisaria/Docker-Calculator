9df706b74f9d9742282653d7eeeac4dfc607e2ab
Payal Ramsisaria <payalramsisaria02@gmail.com>
2026-06-09 13:42:34 +0530

    Docker Calculator Project

---
+++ calculator.py
@@ -0,0 +1,23 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter First Number: "))
+operator = input("Enter Operator (+,-,*,/): ")
+num2 = float(input("Enter Second Number: "))
+
+if operator == "+":
+    print("Result =", num1 + num2)
+
+elif operator == "-":
+    print("Result =", num1 - num2)
+
+elif operator == "*":
+    print("Result =", num1 * num2)
+
+elif operator == "/":
+    if num2 != 0:
+        print("Result =", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid Operator")
